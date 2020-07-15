4abc47cee3236c8924ebfdd1ce80924cc4c33340
HuangYuhui <Gentleman_0109@outlook.com>
2020-07-15 11:41:43 +0800

    🎉 hello c

---
+++ data-structures-and-algorithms/C/Hello.c
@@ -0,0 +1,13 @@
+/*
+ * @Author: Goog Tech
+ * @Date: 2020-07-15 11:33:42
+ * @Description: test by the plugin of Code Runner
+ * @FilePath: \leetcode-googtech\data-structures-and-algorithms\C\Hello.c
+ */ 
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    print('Hello C')
+    return 0;
+}
