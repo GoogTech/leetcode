2cd12247f4661b7372f59df33f243078de13b957
HuangYuhui <Gentleman_0109@outlook.com>
2020-07-15 11:15:52 +0800

    🎉 hello golang

---
+++ data-structures-and-algorithms/Go/Hello.go
@@ -0,0 +1,13 @@
+/*
+ * @Author: Goog Tech
+ * @Date: 2020-07-15 11:06:39
+ * @Description: It's my first code of Golang and test by the plugin of Code Runner
+ * @FilePath: \leetcode-googtech\data-structures-and-algorithms\Go\Hello.go
+ */ 
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("Hello Golang")
+}
