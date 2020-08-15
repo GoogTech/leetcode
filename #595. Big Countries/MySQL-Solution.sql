b2863f47a7773f9b6c843ce77ee7cc15aadd87d8
HuangYuhui <Gentleman_0109@outlook.com>
2020-08-15 15:32:56 +0800

    🎉 #595. Big Countries

---
+++ #595. Big Countries/MySQL-Solution.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT name, population, area FROM world WHERE area > 3000000 or population > 25000000
---
+++ #595. Big Countries/SQLServer-Solution.sql
@@ -0,0 +1,2 @@
+/* Write your T-SQL query statement below */
+SELECT name, population, area FROM world WHERE area > 3000000 or population > 25000000
