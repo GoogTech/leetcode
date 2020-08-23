b454987baccdc2a7a99c4540477a4173e75734cf
HuangYuhui <Gentleman_0109@outlook.com>
2020-08-23 22:29:32 +0800

    🎉 #14. Longest Common Prefix

---
+++ #14. Longest Common Prefix/Solution.java
@@ -0,0 +1,22 @@
+/*
+ * @Author: Goog Tech
+ * @Date: 2020-08-23 21:57:58
+ * @LastEditTime: 2020-08-23 22:28:04
+ * @Description: https://leetcode-cn.com/problems/longest-common-prefix/
+ * @FilePath: \leetcode-googtech\#102. Binary Tree Level Order Traversal\14.最长公共前缀.java
+ * @WebSite: https://algorithm.show/
+ */
+
+/*
+ * @lc app=leetcode.cn id=14 lang=java
+ *
+ * [14] 最长公共前缀
+ */
+
+// @lc code=start
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        // 待解. . .
+    }
+}
+// @lc code=end
