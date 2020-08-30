c34dcbb8c7042e5767a52bf096aef33bf0cca058
HuangYuhui <Gentleman_0109@outlook.com>
2020-08-30 20:03:59 +0800

    🎉 #925. Long Pressed Name

---
+++ #925. Long Pressed Name/Solution.py
@@ -0,0 +1,18 @@
+'''
+Author: Goog Tech
+Date: 2020-08-30 20:00:20
+LastEditTime: 2020-08-30 20:01:52
+Description: https://leetcode-cn.com/problems/long-pressed-name/
+FilePath: \leetcode-googtech\#925. Long Pressed Name\Solution.py
+WebSite: https://algorithm.show/
+'''
+
+class Solution(object):
+    def isLongPressedName(self, name, typed):
+        """
+        :type name: str
+        :type typed: str
+        :rtype: bool
+        """
+
+        ''' 待解. . . '''
